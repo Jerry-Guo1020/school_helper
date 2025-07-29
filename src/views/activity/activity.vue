3abb553b54e2766e6935171e9bd15b21959dcfc6
Jerry-Guo1020 <2593517388@qq.com>
2025-07-30 00:05:19 +0800

    feat：规划了一下项目结构

---
+++ src/views/activity/activity.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="contanier">
+    hello,this is activity page
+    <Navbar />
+</div>
+</template>
+
+<script setup>
+import Navbar from '../../components/Navbar/navbar.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/message/message.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="contanier">
+    hello,this is message page
+    <Navbar />
+</div>
+</template>
+
+<script setup>
+import Navbar from '../../components/Navbar/navbar.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
