90b0bb5c94670a66542068e0f0b43b27efed2d8f
Jerry-Guo1020 <2593517388@qq.com>
2025-08-02 00:02:30 +0800

    feat：初始化了消息的功能页面

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,12 +1,15 @@
 <template>
 <div class="contanier">
-    hello,this is message page
+    <div class="title">
+        <TopNavbar title="消息"/>
+    </div>
     <Navbar />
 </div>
 </template>
 
 <script setup>
 import Navbar from '../../components/Navbar/navbar.vue';
+import TopNavbar from '../../components/Navbar/topNavbar.vue';
 
 
 </script>
